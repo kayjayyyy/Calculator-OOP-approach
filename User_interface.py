9d77c168241f7ae20fed04176e5a0546fe95721c
unknown <templanzatintin@gmail.com>
2023-05-25 00:23:00 +0800

    define operations

---
+++ User_interface.py
@@ -0,0 +1,6 @@
+class user_interface:
+    
+    def operations(self):
+        math = input("\n\033[33;3mPlease choose a math operation you want to use (a/b/c/d): \033[0m")
+        return math
+    
