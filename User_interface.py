7e7ce6cb8a6f3ce9feaf3ccd34710ce4fee98dc8
unknown <templanzatintin@gmail.com>
2023-05-25 00:24:17 +0800

    define second number

--- User_interface.py
+++ User_interface.py
@@ -7,4 +7,8 @@ class user_interface:
     def num1(self):
         first_number = float(input("\n\033[33;3mEnter your first number: \033[0m"))
         return first_number
+        
+    def num2(self):
+        second_number = float(input("\n\033[33;3mEnter your second number: \033[0m"))
+        return second_number
     
