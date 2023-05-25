134e1512079e2a118be543cf77a3ff8c5645f1ac
unknown <templanzatintin@gmail.com>
2023-05-25 16:34:23 +0800

    greeting

--- CalculatorV2.py
+++ CalculatorV2.py
@@ -1,3 +1,9 @@
 # Templanza, Kristine Joy F.
 # BSCPE 1-4
 # Asignnment no. 7 - Calculator V2 (OOP approach)
+
+# Greeting and border line
+print("")
+print("\033[35m※ \033[0m" * 40)
+print("")
+print("\033[45m ♥ Welcome to our calculator program! ♥ \033[0m".center(85))
