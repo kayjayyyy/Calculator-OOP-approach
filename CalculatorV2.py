325085d01b28ad47ec9f03a25970140e71e11e42
unknown <templanzatintin@gmail.com>
2023-05-25 16:35:15 +0800

    ask the users name

--- CalculatorV2.py
+++ CalculatorV2.py
@@ -7,3 +7,8 @@ print("")
 print("\033[35m※ \033[0m" * 40)
 print("")
 print("\033[45m ♥ Welcome to our calculator program! ♥ \033[0m".center(85))
+
+# Ask the name of the user 
+name = input("\n\033[033mGood day! What is your name? \033[0m")
+print("\n\033[3;33mI hope you are doing well,", name + "!\033[0m")
+print("")
