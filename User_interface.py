b9eac07d033a40ae302000b2e8d9f3df3fb7f6e0
unknown <templanzatintin@gmail.com>
2023-06-03 22:25:30 +0800

    typo fixed

--- User_interface.py
+++ User_interface.py
@@ -5,7 +5,9 @@ class user_interface:
         print("\033[36mb. Division\033[0m".center(85))
         print("\033[36mc. Addition\033[0m".center(85))
         print("\033[36md. Subtraction\033[0m".center(85))
-        math = input("\n\033[33;3mPlease choose a math operation you want to use (a/b/c/d): \033[0m")
+        print("\033[36me. Power or exponent\033[0m".center(85))
+        print("\033[36mf. Modulo\033[0m".center(85))
+        math = input("\n\033[33;3mPlease choose a math operation you want to use (a/b/c/d/e/f): \033[0m")
         return math
     
     def num1(self):
