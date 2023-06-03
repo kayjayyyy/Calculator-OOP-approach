34da5ddfd23aef0b9a259d6fddf60d2529716e24
unknown <templanzatintin@gmail.com>
2023-06-03 21:38:49 +0800

    create new user interface and define total powert and modulo

---
+++ New_user_interface.py
@@ -0,0 +1,9 @@
+from User_interface import user_interface
+
+class NewInterface(user_interface):
+    
+    def total_power(self, power):
+        print("\n\033[32mThe calculated total is: ", power, "\033[0m")
+        
+    def total_modulo(self, total)
+        print("\n\033[32mThe calculated total is: ", total, "\033[0m")
