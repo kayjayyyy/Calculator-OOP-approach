b2bd6d2c3fce11291de81cb2d949fccd982dcb5f
unknown <templanzatintin@gmail.com>
2023-06-03 20:50:13 +0800

    define new function; power exponent

--- Calculator_function.py
+++ Calculator_function.py
@@ -1,2 +1,7 @@
 from ClassCalc import calculator_program
 
+class AddedFunction(calculator_program):
+    
+    def power_exponent(self, first_number, second_number):
+        power = first_number ** second_number
+        return power
